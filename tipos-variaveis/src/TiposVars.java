66f549906c0482e33c5b3c30a4432eea3df38b4b
Rafael <rafapadovan07@gmail.com>
2024-08-25 22:49:50 -0300

    variaveis em java

--- tipos-variaveis/src/TiposVars.java
+++ tipos-variaveis/src/TiposVars.java
@@ -1,5 +1,13 @@
 public class TiposVars {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 1500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        long numeroLongo = 10000000000000000L;
+        int numero = 5;
+        numero = 10;
+        final double VALOR_DE_PI = 3.1415;//constante e uppercase
+
     }
 }
