653b23745c223461ea37e4a11a531d14f19949e9
Rafael <rafapadovan07@gmail.com>
2024-08-29 19:08:33 -0300

    try e catch

---
+++ java-terminal/src/Excessoes.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+public class Excessoes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try{
+            System.out.println("Digite seu nome:");
+            String nome = scanner.next(); 
+            
+            System.out.println("Digite seu sobrenome");
+            String sobrenome =  scanner.next();
+
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
+
+
+            System.out.println(nome + " " + sobrenome);
+            System.out.println(idade);
+            System.out.println(altura);
+            scanner.close();
+    }catch(InputMismatchException e){
+            System.out.println("Digite os campos de idade de altura com elementos numéricos");
+    }
+
+    }
+}
